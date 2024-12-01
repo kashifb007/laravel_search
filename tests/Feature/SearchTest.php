5d1a35e450438338144023c81407507262de059c
Kashif Bhatti <kash@dreamsites.co.uk>
2024-12-01 12:38:14 +0000

    fix: correction to tests, all passed

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -1,11 +1,42 @@
 <?php
+uses(Tests\TestCase::class);
 
 test('no query validation error', function () {
-    $response = $this->post('/ajaxSearchVue', [
+    $response = $this->post('/search', [
         'query' => null,
         'sort' => 'asc',
         'offSet' => 0,
     ]);
 
-    $response->assertStatus(422); // validation failed
+    $response->assertStatus(302); // validation failed
+});
+
+test('search returns data', function () {
+    $repository = new \App\Repositories\SearchRepository;
+    $action = new \App\Actions\Search($repository);
+    $dto = new \App\DataTransferObjects\SearchDto('ipad', 0, 'asc');
+    $data = $action->handle($dto);
+    $count = count($data);
+
+    expect($count)->toBeGreaterThan(0);
+});
+
+test('search returns data without sort parameter', function () {
+    $repository = new \App\Repositories\SearchRepository;
+    $action = new \App\Actions\Search($repository);
+    $dto = new \App\DataTransferObjects\SearchDto('ipad', 0, null);
+    $data = $action->handle($dto);
+    $count = count($data);
+
+    expect($count)->toBeGreaterThan(0);
+});
+
+test('search returns NO data', function () {
+    $repository = new \App\Repositories\SearchRepository;
+    $action = new \App\Actions\Search($repository);
+    $dto = new \App\DataTransferObjects\SearchDto('adsadsadsa', 0, 'asc');
+    $data = $action->handle($dto);
+    $count = count($data);
+
+    expect($count)->toBe(0);
 });
