f055bdca1febf59428dcc72445c2dba80b7d0e5b
Kashif Bhatti <kash@dreamsites.co.uk>
2024-12-13 23:09:13 +0000

    feat: added prettier formatting

--- resources/js/components/SearchImage.vue
+++ resources/js/components/SearchImage.vue
@@ -1,13 +1,32 @@
 <template>
-    <a href="#" class="search__link">
+    <a
+        href="#"
+        class="search__link"
+    >
         <div class="image__container">
-            <img v-bind:src="'/image/' + item_image" class="search__image" :alt="item_name" />
+            <img
+                v-bind:src="'/image/' + item_image"
+                class="search__image"
+                :alt="item_name"
+            />
         </div>
         <div class="search__copy">
-            <span class="search__product_name" v-text="item_name"></span>
-            <span class="search__product_desc" v-text="item_desc"></span>
+            <span
+                class="search__product_name"
+                v-text="item_name"
+            ></span>
+            <span
+                class="search__product_desc"
+                v-text="item_desc"
+            ></span>
         </div>
-        <div class="search__product-card"><span class="product_label"><strong>Price</strong></span> <span class="product_price" v-text="'£' + item_price"></span></div>
+        <div class="search__product-card"
+            ><span class="product_label"><strong>Price</strong></span>
+            <span
+                class="product_price"
+                v-text="'£' + item_price"
+            ></span
+        ></div>
     </a>
 </template>
 
